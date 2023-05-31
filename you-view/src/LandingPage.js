a2b1c2393a907520ffbaff6362c7a6491eba476a
Kendell789 <kendell.trojan@yahoo.com>
2023-05-31 11:21:11 -0700

    start

---
+++ you-view/src/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './LandingPage.module.css';
+
+
+const LandingPage = () => {
+    return (
+        <div className={styles.landingPage}>
+            <h1>Welcome to Our Website!</h1>
+            <p>This is the landing page for our awesome website. Here, you can learn more about our services and products.</p>
+        </div>
+    );
+};
+
+export default LandingPage;
