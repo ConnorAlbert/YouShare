2198f5d0eca16349ab3632fb23506bf47c351fe8
Connor Albert <connoralbert23@hotmail.com>
2024-09-09 02:05:09 -0500

    Update
    Updated Featured page

--- you-view/src/components/Featured.js
+++ you-view/src/components/Featured.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ThumbUpAltOutlinedIcon from '@mui/icons-material/ThumbUpAltOutlined';
-
+import '../styles/Featured.css'; // Import the CSS file
 
 const extractVideoId = (url) => {
   try {
@@ -91,42 +91,47 @@ const Featured = () => {
   if (!featuredUser) return <p>No featured user found</p>;
 
   return (
-    <div style={{ height: '100%', backgroundColor: '#242F40', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
+    <div className="featured-container">
+      <div className="featured-inner">
         {/* Left Section */}
-        <div style={{ width: '22.5%', display: 'flex', flexDirection: 'column', alignItems: 'center', marginRight: '10px', padding: '20px' }}>
-          <AccountCircleIcon style={styles.icon} />
-          <h3 style={{ margin: '10px 0', color: '#CCA43B', textAlign: 'center', fontSize: '36px' }}>{featuredUser.username}</h3>
-          <div style={{ backgroundColor: '#242F40', borderRadius: '50px', padding: '10px', textAlign: 'center', width: '60%' }}>
-            <p style={{ margin: '5px 0', color: 'white', fontSize: '24px' }}>Current 🔥: 7</p>
-            <p style={{ margin: '5px 0', color: 'white', fontSize: '24px' }}>Highscore 🏆: 12</p>
+        <div className="left-section">
+        <AccountCircleIcon style={{ fontSize: '10rem', color: '#CCA43B' }} />
+          
+          <div className="points-container">
+          <h3 className="username">{featuredUser.username}</h3>
+            <div className="points-box">
+              <p className="points-text">Daily Points: <span className="points-count">7</span></p>
+            </div>
+            <div className="points-box">
+              <p className="points-text">Total Points: <span className="points-count">12</span></p>
+            </div>
           </div>
         </div>
 
         {/* Middle Section */}
-        <div style={{ width: '45%', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <h2 style={{ marginTop: '0', textAlign: 'center', color: '#CCA43B', paddingTop: '10px' }}>Featured Creator</h2>
-          <div style={{ width: '100%', height: '400px', backgroundColor: 'lightgray' }}>
+        <div className="middle-section">
+          <h2 className="title">Featured Creator</h2>
+          <div className="video-container">
             {featuredUser.featuredVideoId && (
               <iframe 
-                width={'100%'}
-                height={'100%'}
+                width="100%"
+                height="100%"
                 src={`https://www.youtube.com/embed/${extractVideoId(featuredUser.featuredVideoId)}?`} 
                 title="YouTube video player" 
                 frameBorder="0" 
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                allowFullScreen> 
+                allowFullScreen>
               </iframe>
             )}
           </div>
         </div>
 
         {/* Right Section */}
-        <div style={{ width: '22.5%', display: 'flex', flexDirection: 'column', alignItems: 'center', marginLeft: '10px', padding: '20px', justifyContent: 'center' }}>
-          <h2 style={{ marginTop: '0', textAlign: 'center', color: '#CCA43B' }}>Actions</h2>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <div className="right-section">
+          <h2 className="title">Actions</h2>
+          <div className="actions-container">
             <button
-              style={checkboxes.like ? { ...styles.button, ...styles.buttonLiked } : { ...styles.button, ...styles.buttonLike }}
+              className={`button buttonLike ${checkboxes.like ? 'buttonLiked' : ''}`}
               onClick={() => handleActionClick('like')}
               disabled={checkboxes.like}
             >
@@ -134,41 +139,38 @@ const Featured = () => {
               {checkboxes.like ? 'Liked' : 'Like'}
             </button>
             <button
-              style={checkboxes.comment ? { ...styles.button, ...styles.buttonCommented } : { ...styles.button, ...styles.buttonComment }}
+              className={`button buttonComment ${checkboxes.comment ? 'buttonCommented' : ''}`}
               onClick={() => handleActionClick('comment')}
               disabled={checkboxes.comment}
             >
               {checkboxes.comment ? 'Commented' : 'Comment'}
             </button>
             <button
-              style={checkboxes.subscribe ? { ...styles.button, ...styles.buttonSubscribed } : { ...styles.button, ...styles.buttonSubscribe }}
+              className={`button buttonSubscribe ${checkboxes.subscribe ? 'buttonSubscribed' : ''}`}
               onClick={() => handleActionClick('subscribe')}
               disabled={checkboxes.subscribe}
             >
               {checkboxes.subscribe ? 'Subscribed' : 'Subscribe'}
             </button>
-            <div style={styles.progressBarContainer}>
-              <div style={{ ...styles.progressBar, width: `${progressWidth}%` }}></div>
-              <span style={styles.progressText}>{Math.round(progressWidth)}%</span>
-            </div>
+            
           </div>
         </div>
       </div>
 
       {/* Verification Modal */}
       {verification.isVisible && (
-        <div style={styles.modalOverlay}>
-          <div style={styles.modal}>
+        <div className="modalOverlay">
+          <div className="modal">
             <h2>{verification.action.charAt(0).toUpperCase() + verification.action.slice(1)} Verification</h2>
             <p>Did you complete the action?</p>
             <button
-              style={{ ...styles.modalButton, ...styles.modalButtonConfirm}}
+              className="modalButton modalButtonConfirm"
               onClick={() => handleVerificationResponse(true)}
             >
               Yes
             </button>
             <button
-              style={{ ...styles.modalButton, ...styles.modalButtonClose }}
+              className="modalButton modalButtonClose"
               onClick={() => handleVerificationResponse(false)}
             >
               No
@@ -180,102 +182,4 @@ const Featured = () => {
   );
 };
 
-const styles = {
-  icon: {
-    fontSize: '12rem',
-  },
-  button: {
-    padding: '10px 20px',
-    border: 'none',
-    borderRadius: '5px',
-    color: 'white',
-    fontSize: '18px',
-    fontWeight: 'bold',
-    transition: 'background-color 0.3s ease',
-    margin: '10px 0',
-    cursor: 'pointer',
-    width: '100%', // Make buttons full width for consistent alignment
-  },
-  buttonLike: {
-    backgroundColor: '#007BFF',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonLiked: {
-    backgroundColor: '#28A745',
-  },
-  buttonComment: {
-    backgroundColor: 'rgb(204, 164, 59)',
-  },
-  buttonCommented: {
-    backgroundColor: '#28A745',
-  },
-  buttonSubscribe: {
-    backgroundColor: 'rgb(231, 14, 23)',
-  },
-  buttonSubscribed: {
-    backgroundColor: '#28A745',
-  },
-  progressBarContainer: {
-    width: '250%', // Full width of the container
-    backgroundColor: '#4C4C4C',
-    borderRadius: '25px',
-    height: '20px',
-    marginTop: '20px',
-    position: 'static',
-    overflow: 'hidden',
-  },
-  progressBar: {
-    height: '100%', // Full height of the container
-    backgroundColor: '#28A745',
-    transition: 'width 0.3s ease',
-    borderRadius: '25px',
-  },
-  progressText: {
-    position: 'absolute',
-    width: '100%',
-    textAlign: 'center',
-    lineHeight: '20px',
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: '14px',
-  },
-  modalOverlay: {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    width: '100vw',
-    height: '100vh',
-    backgroundColor: 'rgba(0, 0, 0, 0.7)',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  modal: {
-    backgroundColor: '#ffffff',
-    padding: '20px',
-    borderRadius: '8px',
-    textAlign: 'center',
-    width: '300px',
-  },
-  modalButton: {
-    padding: '10px 20px',
-    border: 'none',
-    borderRadius: '5px',
-    margin: '5px',
-    cursor: 'pointer',
-    fontSize: '16px',
-    fontWeight: 'bold',
-    color: '#ffffff',
-  },
-  modalButtonConfirm: {
-    backgroundColor: '#28a745',
-  },
-  modalButtonClose: {
-    backgroundColor: '#dc3545',
-  },
-};
-
 export default Featured;
-
