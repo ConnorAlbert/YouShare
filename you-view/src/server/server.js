bc667d3aa55d35d380994bcd192b4e24cb3a0980
Connor Albert <connoralbert23@hotmail.com>
2024-09-12 15:04:00 -0500

    Update server.js

--- you-view/src/server/server.js
+++ you-view/src/server/server.js
@@ -19,7 +19,7 @@ async function startServer() {
   const app = express();
 
   // CORS setup
-  app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+  app.use(cors({ origin: '*', credentials: true }));
 
   // Session setup
   app.use(session({
